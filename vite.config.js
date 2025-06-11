d87323fcc4733084be044c85f797e21d04c2d4f5
nageshmali <malinagesh60@gmail.com>
2025-06-11 19:13:24 +0530

    modified config file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/textalter/"
+  base: "/textalter/",
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // or whatever port you're using
+  }
 })
